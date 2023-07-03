b0b4c15647de07678a41c3a39ac3c18036dad57c
Lukas Krickl <lukas@krickl.dev>
2023-07-03 17:44:40 +0200

    WIP: Rand implementation

--- include/libmuzzy/rand.h
+++ include/libmuzzy/rand.h
@@ -2,13 +2,28 @@
 #define RAND_H_
 
 #include <stdint.h>
+#include <stdio.h>
 
-typedef int64_t (*muzzy_next_rand)(void *data);
+struct muzzy_rand_cfg {
+  union {
+    struct {
+      const char *src_path;
+      FILE *fp;
+    };
+    void *custom;
+  };
+};
+
+struct muzzy_rand_cfg muzzy_rand_cfg_init(void);
+struct muzzy_rand_cfg muzzy_rand_cfg_file(const char *path);
 
 // regular std-c rand call
 int64_t muzzy_rand(void *data);
 
 // read data from file and use them for rand
 int64_t muzzy_frand(void *data);
+
+void muzzy_rand_cfg_free(struct muzzy_rand_cfg *self);
+void muzzy_frand_cfg_free(struct muzzy_rand_cfg *self);
 
 #endif
--- src/libmuzzy/rand.c
+++ src/libmuzzy/rand.c
@@ -1,1 +1,38 @@
 #include "libmuzzy/rand.h"
+#include "libmuzzy/error.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct muzzy_rand_cfg muzzy_rand_cfg_init(void) {
+  struct muzzy_rand_cfg self;
+  memset(&self, 0, sizeof(self));
+
+  return self;
+}
+
+struct muzzy_rand_cfg muzzy_rand_cfg_file(const char *path) {
+  struct muzzy_rand_cfg self = muzzy_rand_cfg_init();
+
+  self.src_path = path;
+  self.fp = fopen(path, "re");
+  if (!self.fp) {
+    muzzy_errno();
+  }
+
+  return self;
+}
+
+int64_t muzzy_rand(void *data) { return rand(); } // NOLINT
+
+int64_t muzzy_frand(void *data) {
+  struct muzzy_rand_cfg *cfg = data;
+  int64_t res = 0;
+  if (fread(&res, 1, sizeof(int64_t), cfg->fp) == -1) {
+    muzzy_errno();
+  }
+
+  return res;
+}
+
+void muzzy_frand_cfg_free(struct muzzy_rand_cfg *self) { fclose(self->fp); }
