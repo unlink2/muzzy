03b8fc868c9ac768e3df7ba28102b48acb22eaec
Lukas Krickl <lukas@krickl.dev>
2023-07-13 20:06:42 +0200

    Fixed test for iter rand

--- src/libmuzzy/test/rand.c
+++ src/libmuzzy/test/rand.c
@@ -10,10 +10,10 @@ void test_iter_rand(void **state) {
   const int max = 4;
   struct muzzy_rand_cfg cfg = muzzy_rand_cfg_iter(max);
 
-  const size_t len = 16;
-  const int64_t tbl0[] = {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
-  const int64_t tbl1[] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
-  const int64_t tbl2[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+  const size_t len = 17;
+  const int64_t tbl0[] = {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0};
+  const int64_t tbl1[] = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 0};
+  const int64_t tbl2[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
 
   for (size_t i = 0; i < len; i++) {
     assert_int_equal(tbl0[i], muzzy_iter_rand(0, &cfg) % max);
